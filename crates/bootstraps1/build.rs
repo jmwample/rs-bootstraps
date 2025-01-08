60385ec5f4d4ef23f5145256ecf7a03bf7df265f
jmwample <8297368+jmwample@users.noreply.github.com>
2025-01-07 17:09:22 -0700

    move naming around

---
+++ crates/bootstraps1/build.rs
@@ -0,0 +1,69 @@
+fn main() {
+    #[cfg(feature = "enable-cfg")]
+    preload::load_hostfile();
+
+    println!("cargo:rerun-if-changed=build.rs");
+}
+
+#[cfg(feature = "enable-cfg")]
+mod preload {
+    // use config_types::Config;
+    // use std::str::FromStr;
+
+    const PREAMBLE: &str = r#"
+    /// Generated Value for configuration hardcoded at compile time
+    pub(crate) const BOOTSTRAP_CONFIG_STR: &str = r#""#;
+
+    macro_rules! warn {
+        ($($tokens: tt)*) => {
+            println!("cargo:warning={}", format!($($tokens)*))
+        }
+    }
+
+    pub(crate) fn load_hostfile() {
+        // allow the name of the file we draw hardcoded values from to be set by an
+        // environment variable at compile time.
+        let cfg_file_name = option_env!("NYMVPN_HOSTFILE_CONFIG").unwrap_or("nymvpn-config.toml");
+
+        // set reasons to rebuild
+        println!("cargo:rerun-if-changed={cfg_file_name}");
+        println!("cargo:rerun-if-env-changed=NYMVPN_HOSTFILE_CONFIG");
+
+        let path = std::path::PathBuf::from(cfg_file_name);
+        let cfg_file_path = if path.is_absolute() {
+            path
+        } else {
+            // CARGO_WORKSPACE_DIR is set in .cargo/config.toml - it is NOT provided by cargo itself
+            let workspace_path = std::env::var("CARGO_WORKSPACE_DIR").unwrap();
+            std::path::Path::new(&workspace_path).join(cfg_file_name)
+        };
+
+        #[cfg(feature = "require-cfg")]
+        let config_str: String = std::fs::read_to_string(cfg_file_path).unwrap();
+
+        #[cfg(not(feature = "require-cfg"))]
+        let config_str: String = match std::fs::read_to_string(cfg_file_path) {
+            // split the string into an iterator of string slices
+            Ok(r) => r,
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::NotFound => {
+                    warn!("config bootstrapping was enabled, but no config file was found");
+                    String::new()
+                }
+                _ => panic!("{e}"),
+            },
+        };
+
+        // let config = Config::from_str(&config_str).unwrap();
+
+        // // creating a string with the values from our config file and the proper length
+        let mut array_string = String::from(PREAMBLE);
+        array_string.push_str(&config_str);
+        array_string.push_str("\"#;\r\n");
+
+        // write the string to a file. OUT_DIR environment variable is defined by cargo
+        let out_dir = std::env::var("OUT_DIR").unwrap();
+        let dest_path = std::path::Path::new(&out_dir).join("obfuscated.rs");
+        std::fs::write(&dest_path, array_string).unwrap();
+    }
+}
--- crates/bootstraps2/src/lib.rs
+++ crates/bootstraps2/src/lib.rs
@@ -1,45 +1,12 @@
 use config_types::BaseConfig;
-use std::str::FromStr;
-
-#[cfg(feature = "enable-cfg")]
-include!(concat!(env!("OUT_DIR"), "/obfuscated.rs"));
 
 pub struct Config(BaseConfig);
 
-/// Default Configuration used if no bootstrap configuration file is provided at compile time.
-const DEFAULT_CONFIG_STR: &str = r#"
-ip = "192.168.1.1"
-port = 4433
-
-[keys]
-github = "00000000000000000"
-travis = "11111111111111111"
-"#;
+include!(concat!(env!("OUT_DIR"), "/default.rs"));
 
 impl AsRef<BaseConfig> for Config {
     fn as_ref(&self) -> &BaseConfig {
         &self.0
-    }
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        #[cfg(feature = "require-cfg")]
-        let config_str = BOOTSTRAP_CONFIG_STR;
-
-        #[cfg(all(not(feature = "require-cfg"), feature = "enable-cfg"))]
-        let config_str = if !BOOTSTRAP_CONFIG_STR.is_empty() {
-            BOOTSTRAP_CONFIG_STR
-        } else {
-            DEFAULT_CONFIG_STR
-        };
-
-        #[cfg(not(feature = "enable-cfg"))]
-        let config_str = DEFAULT_CONFIG_STR;
-
-        let config = BaseConfig::from_str(config_str).expect("failed to parse default config");
-
-        Self(config)
     }
 }
 
@@ -51,7 +18,6 @@ mod tests {
     fn it_works() {
         #[cfg(feature = "enable-cfg")]
         {
-            println!("{:?}", BOOTSTRAP_CONFIG_STR);
             let c = Config::default();
             let config = c.as_ref();
             assert_eq!(config.ip, "127.0.0.1");
